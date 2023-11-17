c7badc7f8a2cff21f951f6edc667e2bbd6285c3b
Yagmur Curku <yagmurcurku@gmail.com>
2023-11-18 01:23:02 +0300

    created Car and Model classes

--- src/main/java/com/tobeto/rentACar/Car.java
+++ src/main/java/com/tobeto/rentACar/Car.java
@@ -1,4 +1,27 @@
 package com.tobeto.rentACar;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "cars")
 public class Car {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+
+    @Column(name = "plate")
+    private String plate;
+
+    @Column(name = "daily_price")
+    private double daily_price;
+
+    @Column(name = "state")
+    private boolean state;
+
+    @ManyToOne
+    @JoinColumn(name = "model_id")
+    private Model model;
+
 }
--- src/main/java/com/tobeto/rentACar/Model.java
+++ src/main/java/com/tobeto/rentACar/Model.java
@@ -1,4 +1,23 @@
 package com.tobeto.rentACar;
 
+import jakarta.persistence.*;
+
+import java.util.List;
+
+@Entity
+@Table(name = "models")
 public class Model {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "year")
+    private int year;
+
+    @OneToMany(mappedBy = "model")
+    private List<Car> cars;
 }
