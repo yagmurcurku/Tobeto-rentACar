fcad398f54befa4ccd1c2699be055f3571578bba
Yagmur Curku <yagmurcurku@gmail.com>
2023-11-18 01:29:10 +0300

    created Brand class

---
+++ src/main/java/com/tobeto/rentACar/entities/Brand.java
@@ -0,0 +1,21 @@
+package com.tobeto.rentACar.entities;
+
+import jakarta.persistence.*;
+
+import java.util.List;
+
+@Entity
+@Table(name = "brands")
+public class Brand {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+
+    @Column(name = "name")
+    private String name;
+
+    @OneToMany(mappedBy = "brand")
+    private List<Model> models;
+}
