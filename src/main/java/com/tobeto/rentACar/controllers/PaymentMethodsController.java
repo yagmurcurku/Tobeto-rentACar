34d51ff812dee32b8c07342da18ddf29860610a3
Yagmur Curku <yagmurcurku@gmail.com>
2023-11-21 22:09:36 +0300

    created PaymentMethodsController

---
+++ src/main/java/com/tobeto/rentACar/controllers/PaymentMethodsController.java
@@ -0,0 +1,41 @@
+package com.tobeto.rentACar.controllers;
+
+import com.tobeto.rentACar.entities.PaymentMethod;
+import com.tobeto.rentACar.repositories.PaymentMethodRepository;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("api/paymentmethods")
+public class PaymentMethodsController {
+
+    private final PaymentMethodRepository paymentMethodRepository;
+
+    public PaymentMethodsController(PaymentMethodRepository paymentMethodRepository){
+        this.paymentMethodRepository=paymentMethodRepository;
+    }
+
+    @GetMapping
+    public List<PaymentMethod> getAll(){
+        return paymentMethodRepository.findAll();
+    }
+
+    @GetMapping("{id}")
+    public PaymentMethod getById(@PathVariable PaymentMethod paymentMethod){
+        return paymentMethodRepository.findById(paymentMethod.getId()).orElseThrow();
+    }
+
+    @PostMapping
+    public void add(@RequestBody PaymentMethod paymentMethod){
+        paymentMethodRepository.save(paymentMethod);
+    }
+
+    @PutMapping
+    public void update(@RequestBody PaymentMethod paymentMethod){
+        PaymentMethod paymentMethodToUpdate = paymentMethodRepository.findById(paymentMethod.getId()).orElseThrow();
+        paymentMethodToUpdate.setName(paymentMethod.getName());
+        paymentMethodToUpdate.setInvoices(paymentMethod.getInvoices());
+    }
+
+}
