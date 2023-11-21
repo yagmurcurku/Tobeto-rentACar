ce068bb020477ef394b51c91f6a850b43d118ec2
Yagmur Curku <yagmurcurku@gmail.com>
2023-11-21 19:20:14 +0300

    created RoleRepository

---
+++ src/main/java/com/tobeto/rentACar/repositories/RoleRepository.java
@@ -0,0 +1,8 @@
+package com.tobeto.rentACar.repositories;
+
+import com.tobeto.rentACar.entities.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RoleRepository extends JpaRepository<Role,Integer> {
+
+}
