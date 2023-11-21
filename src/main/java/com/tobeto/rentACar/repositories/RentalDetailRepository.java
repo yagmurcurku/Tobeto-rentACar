c8da28b947062d65ba8722b8ccbc8a824cc85556
Yagmur Curku <yagmurcurku@gmail.com>
2023-11-21 14:41:06 +0300

    created RentalDetailRepository

---
+++ src/main/java/com/tobeto/rentACar/repositories/RentalDetailRepository.java
@@ -0,0 +1,8 @@
+package com.tobeto.rentACar.repositories;
+
+import com.tobeto.rentACar.entities.Rental;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RentalDetailRepository extends JpaRepository<Rental,Integer> {
+
+}
