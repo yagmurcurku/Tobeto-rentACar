3b7f704d17758ea10c68cb621462fd30acb84a5b
Yagmur Curku <yagmurcurku@gmail.com>
2023-11-23 17:05:08 +0300

    created employee requests and responses

--- src/main/java/com/tobeto/rentACar/controllers/EmployeesController.java
+++ src/main/java/com/tobeto/rentACar/controllers/EmployeesController.java
@@ -1,9 +1,15 @@
 package com.tobeto.rentACar.controllers;
 
+import com.tobeto.rentACar.dtos.requests.employee.AddEmployeeRequest;
+import com.tobeto.rentACar.dtos.requests.employee.UpdateEmployeeRequest;
+import com.tobeto.rentACar.dtos.responses.employee.GetEmployeeListResponse;
+import com.tobeto.rentACar.dtos.responses.employee.GetEmployeeResponse;
+import com.tobeto.rentACar.dtos.responses.location.GetLocationListResponse;
 import com.tobeto.rentACar.entities.Employee;
 import com.tobeto.rentACar.repositories.EmployeeRepository;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -17,34 +23,69 @@ public class EmployeesController {
     }
 
     @GetMapping
-    public List<Employee> getAll(){
-        return employeeRepository.findAll();
+    public List<GetEmployeeListResponse> getAll(){
+        List<Employee> employeeList = employeeRepository.findAll();
+        List<GetEmployeeListResponse> employeeListResponses = new ArrayList<GetEmployeeListResponse>();
+        for (Employee employee: employeeList) {
+            GetEmployeeListResponse employeeResponse = new GetEmployeeListResponse();
+            employeeResponse.setId(employee.getId());
+            employeeResponse.setFirstName(employee.getFirstName());
+            employeeResponse.setLastName(employee.getLastName());
+            employeeResponse.setPhone(employee.getPhone());
+            employeeResponse.setEmail(employee.getEmail());
+            employeeResponse.setBirthDate(employee.getBirthDate());
+            employeeResponse.setGender(employee.getGender());
+            employeeResponse.setIdentificationNumber(employee.getIdentificationNumber());
+            employeeResponse.setLocation(employee.getLocation());
+            employeeResponse.setRole(employee.getRole());
+            employeeListResponses.add(employeeResponse);
+        }
+        return employeeListResponses;
     }
 
     @GetMapping("{id}")
-    public Employee getById(@PathVariable int id){
-        return employeeRepository.findById(id).orElseThrow();
+    public GetEmployeeResponse getById(@PathVariable int id){
+        Employee employee = employeeRepository.findById(id).orElseThrow();
+        GetEmployeeResponse dto = new GetEmployeeResponse();
+        dto.setFirstName(employee.getFirstName());
+        dto.setLastName(employee.getLastName());
+        dto.setPhone(employee.getPhone());
+        dto.setEmail(employee.getEmail());
+        dto.setBirthDate(employee.getBirthDate());
+        dto.setGender(employee.getGender());
+        dto.setIdentificationNumber(employee.getIdentificationNumber());
+        dto.setLocation(employee.getLocation());
+        dto.setRole(employee.getRole());
+        return dto;
     }
 
     @PostMapping
-    public void add(@RequestBody Employee employee){
+    public void add(@RequestBody AddEmployeeRequest addEmployeeRequest){
+        Employee employee = new Employee();
+        employee.setFirstName(addEmployeeRequest.getFirstName());
+        employee.setLastName(addEmployeeRequest.getLastName());
+        employee.setPhone(addEmployeeRequest.getPhone());
+        employee.setEmail(addEmployeeRequest.getEmail());
+        employee.setBirthDate(addEmployeeRequest.getBirthDate());
+        employee.setGender(addEmployeeRequest.getGender());
+        employee.setIdentificationNumber(addEmployeeRequest.getIdentificationNumber());
+        employee.setLocation(addEmployeeRequest.getLocation());
+        employee.setRole(addEmployeeRequest.getRole());
         employeeRepository.save(employee);
     }
 
     @PutMapping
-    public void update(@RequestBody Employee employee){
-        Employee employeeToUpdate = employeeRepository.findById(employee.getId()).orElseThrow();
-        employeeToUpdate.setFirstName(employee.getFirstName());
-        employeeToUpdate.setLastName(employee.getLastName());
-        employeeToUpdate.setPhone(employee.getPhone());
-        employeeToUpdate.setEmail(employee.getEmail());
-        employeeToUpdate.setBirthDate(employee.getBirthDate());
-        employeeToUpdate.setGender(employee.getGender());
-        employeeToUpdate.setIdentificationNumber(employee.getIdentificationNumber());
-        employeeToUpdate.setLocation(employee.getLocation());
-        employeeToUpdate.setRole(employee.getRole());
-        employeeToUpdate.setInvoices(employee.getInvoices());
-        employeeToUpdate.setRentalDetails(employee.getRentalDetails());
+    public void update(@RequestBody UpdateEmployeeRequest updateEmployeeRequest){
+        Employee employeeToUpdate = employeeRepository.findById(updateEmployeeRequest.getId()).orElseThrow();
+        employeeToUpdate.setFirstName(updateEmployeeRequest.getFirstName());
+        employeeToUpdate.setLastName(updateEmployeeRequest.getLastName());
+        employeeToUpdate.setPhone(updateEmployeeRequest.getPhone());
+        employeeToUpdate.setEmail(updateEmployeeRequest.getEmail());
+        employeeToUpdate.setBirthDate(updateEmployeeRequest.getBirthDate());
+        employeeToUpdate.setGender(updateEmployeeRequest.getGender());
+        employeeToUpdate.setIdentificationNumber(updateEmployeeRequest.getIdentificationNumber());
+        employeeToUpdate.setLocation(updateEmployeeRequest.getLocation());
+        employeeToUpdate.setRole(updateEmployeeRequest.getRole());
         employeeRepository.save(employeeToUpdate);
     }
 
---
+++ src/main/java/com/tobeto/rentACar/dtos/requests/employee/UpdateEmployeeRequest.java
@@ -0,0 +1,27 @@
+package com.tobeto.rentACar.dtos.requests.employee;
+
+import com.tobeto.rentACar.entities.Location;
+import com.tobeto.rentACar.entities.Role;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class UpdateEmployeeRequest {
+
+    private int id;
+    private String firstName;
+    private String lastName;
+    private String phone;
+    private String email;
+    private LocalDate birthDate;
+    private String gender;
+    private String identificationNumber;
+    private Location location;
+    private Role role;
+
+}
