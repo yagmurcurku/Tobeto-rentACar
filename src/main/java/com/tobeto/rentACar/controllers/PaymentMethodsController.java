ee2b7df79b8c6a5a61f685175c77dc01c15dab5b
Yagmur Curku <yagmurcurku@gmail.com>
2023-11-23 15:44:27 +0300

    created paymentMethod requests and responses

--- src/main/java/com/tobeto/rentACar/controllers/PaymentMethodsController.java
+++ src/main/java/com/tobeto/rentACar/controllers/PaymentMethodsController.java
@@ -1,9 +1,14 @@
 package com.tobeto.rentACar.controllers;
 
+import com.tobeto.rentACar.dtos.requests.paymentMethod.AddPaymentMethodRequest;
+import com.tobeto.rentACar.dtos.requests.paymentMethod.UpdatePaymentMethodRequest;
+import com.tobeto.rentACar.dtos.responses.paymentMethod.GetPaymentMethodListResponse;
+import com.tobeto.rentACar.dtos.responses.paymentMethod.GetPaymentMethodResponse;
 import com.tobeto.rentACar.entities.PaymentMethod;
 import com.tobeto.rentACar.repositories.PaymentMethodRepository;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -17,25 +22,37 @@ public class PaymentMethodsController {
     }
 
     @GetMapping
-    public List<PaymentMethod> getAll(){
-        return paymentMethodRepository.findAll();
+    public List<GetPaymentMethodListResponse> getAll(){
+        List<PaymentMethod> paymentMethodList = paymentMethodRepository.findAll();
+        List<GetPaymentMethodListResponse> paymentMethodListResponses = new ArrayList<GetPaymentMethodListResponse>();
+        for (PaymentMethod paymentMethod: paymentMethodList) {
+            GetPaymentMethodListResponse paymentMethodResponse = new GetPaymentMethodListResponse();
+            paymentMethodResponse.setId(paymentMethod.getId());
+            paymentMethodResponse.setName(paymentMethod.getName());
+            paymentMethodListResponses.add(paymentMethodResponse);
+        }
+        return paymentMethodListResponses;
     }
 
     @GetMapping("{id}")
-    public PaymentMethod getById(@PathVariable int id){
-        return paymentMethodRepository.findById(id).orElseThrow();
+    public GetPaymentMethodResponse getById(@PathVariable int id){
+        PaymentMethod paymentMethod = paymentMethodRepository.findById(id).orElseThrow();
+        GetPaymentMethodResponse dto = new GetPaymentMethodResponse();
+        dto.setName(paymentMethod.getName());
+        return dto;
     }
 
     @PostMapping
-    public void add(@RequestBody PaymentMethod paymentMethod){
+    public void add(@RequestBody AddPaymentMethodRequest addPaymentMethodRequest){
+        PaymentMethod paymentMethod = new PaymentMethod();
+        paymentMethod.setName(addPaymentMethodRequest.getName());
         paymentMethodRepository.save(paymentMethod);
     }
 
     @PutMapping
-    public void update(@RequestBody PaymentMethod paymentMethod){
-        PaymentMethod paymentMethodToUpdate = paymentMethodRepository.findById(paymentMethod.getId()).orElseThrow();
-        paymentMethodToUpdate.setName(paymentMethod.getName());
-        paymentMethodToUpdate.setInvoices(paymentMethod.getInvoices());
+    public void update(@RequestBody UpdatePaymentMethodRequest updatePaymentMethodRequest){
+        PaymentMethod paymentMethodToUpdate = paymentMethodRepository.findById(updatePaymentMethodRequest.getId()).orElseThrow();
+        paymentMethodToUpdate.setName(updatePaymentMethodRequest.getName());
         paymentMethodRepository.save(paymentMethodToUpdate);
     }
 
---
+++ src/main/java/com/tobeto/rentACar/dtos/requests/paymentMethod/AddPaymentMethodRequest.java
@@ -0,0 +1,14 @@
+package com.tobeto.rentACar.dtos.requests.paymentMethod;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class AddPaymentMethodRequest {
+
+    private String name;
+
+}
