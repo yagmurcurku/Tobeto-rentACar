bf4b95548d3294bc237cd6d5cba75486742f73f1
Yagmur Curku <yagmurcurku@gmail.com>
2023-11-29 21:41:57 +0300

    created LocationManager

--- src/main/java/com/tobeto/rentACar/controllers/LocationsController.java
+++ src/main/java/com/tobeto/rentACar/controllers/LocationsController.java
@@ -1,11 +1,13 @@
 package com.tobeto.rentACar.controllers;
 
+import com.tobeto.rentACar.services.abstracts.LocationService;
 import com.tobeto.rentACar.services.dtos.requests.location.AddLocationRequest;
 import com.tobeto.rentACar.services.dtos.requests.location.UpdateLocationRequest;
 import com.tobeto.rentACar.services.dtos.responses.location.GetLocationListResponse;
 import com.tobeto.rentACar.services.dtos.responses.location.GetLocationResponse;
 import com.tobeto.rentACar.entities.Location;
 import com.tobeto.rentACar.repositories.LocationRepository;
+import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
@@ -13,60 +15,34 @@ import java.util.List;
 
 @RestController
 @RequestMapping("api/locations")
+@AllArgsConstructor
 public class LocationsController {
 
-    private final LocationRepository locationRepository;
-
-    public LocationsController(LocationRepository locationRepository){
-        this.locationRepository=locationRepository;
-    }
+    private final LocationService locationService;
 
     @GetMapping
     public List<GetLocationListResponse> getAll(){
-        List<Location> locationList = locationRepository.findAll();
-        List<GetLocationListResponse> locationListResponses = new ArrayList<GetLocationListResponse>();
-        for (Location location: locationList) {
-            GetLocationListResponse locationResponse = new GetLocationListResponse();
-            locationResponse.setId(location.getId());
-            locationResponse.setCity(location.getCity());
-            locationResponse.setBranchOffice(location.getBranchOffice());
-            locationResponse.setOfficePhone(location.getOfficePhone());
-            locationListResponses.add(locationResponse);
-        }
-        return locationListResponses;
+        return locationService.getAll();
     }
 
     @GetMapping("{id}")
     public GetLocationResponse getById(@PathVariable int id){
-        Location location = locationRepository.findById(id).orElseThrow();
-        GetLocationResponse dto = new GetLocationResponse();
-        dto.setCity(location.getCity());
-        dto.setBranchOffice(location.getBranchOffice());
-        dto.setOfficePhone(location.getOfficePhone());
-        return dto;
+        return locationService.getById(id);
     }
 
     @PostMapping
     public void add(@RequestBody AddLocationRequest addLocationRequest){
-        Location location = new Location();
-        location.setCity(addLocationRequest.getCity());
-        location.setBranchOffice(addLocationRequest.getBranchOffice());
-        location.setOfficePhone(addLocationRequest.getOfficePhone());
-        locationRepository.save(location);
+        locationService.add(addLocationRequest);
     }
 
     @PutMapping
     public void update(@RequestBody UpdateLocationRequest updateLocationRequest){
-        Location locationToUpdate = locationRepository.findById(updateLocationRequest.getId()).orElseThrow();
-        locationToUpdate.setCity(updateLocationRequest.getCity());
-        locationToUpdate.setBranchOffice(updateLocationRequest.getBranchOffice());
-        locationToUpdate.setOfficePhone(updateLocationRequest.getOfficePhone());
-        locationRepository.save(locationToUpdate);
+        locationService.update(updateLocationRequest);
     }
 
     @DeleteMapping("{id}")
     public void delete(@PathVariable int id){
-        locationRepository.deleteById(id);
+        locationService.delete(id);
     }
 
 }
---
+++ src/main/java/com/tobeto/rentACar/services/concretes/LocationManager.java
@@ -0,0 +1,70 @@
+package com.tobeto.rentACar.services.concretes;
+
+import com.tobeto.rentACar.entities.Location;
+import com.tobeto.rentACar.repositories.LocationRepository;
+import com.tobeto.rentACar.services.abstracts.LocationService;
+import com.tobeto.rentACar.services.dtos.requests.location.AddLocationRequest;
+import com.tobeto.rentACar.services.dtos.requests.location.UpdateLocationRequest;
+import com.tobeto.rentACar.services.dtos.responses.location.GetLocationListResponse;
+import com.tobeto.rentACar.services.dtos.responses.location.GetLocationResponse;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@AllArgsConstructor
+@Service
+public class LocationManager implements LocationService {
+
+    private final LocationRepository locationRepository;
+
+    @Override
+    public List<GetLocationListResponse> getAll() {
+        List<Location> locationList = locationRepository.findAll();
+        List<GetLocationListResponse> locationListResponses = new ArrayList<GetLocationListResponse>();
+        for (Location location: locationList) {
+            GetLocationListResponse locationResponse = new GetLocationListResponse();
+            locationResponse.setId(location.getId());
+            locationResponse.setCity(location.getCity());
+            locationResponse.setBranchOffice(location.getBranchOffice());
+            locationResponse.setOfficePhone(location.getOfficePhone());
+            locationListResponses.add(locationResponse);
+        }
+        return locationListResponses;
+    }
+
+    @Override
+    public GetLocationResponse getById(int id) {
+        Location location = locationRepository.findById(id).orElseThrow();
+        GetLocationResponse dto = new GetLocationResponse();
+        dto.setCity(location.getCity());
+        dto.setBranchOffice(location.getBranchOffice());
+        dto.setOfficePhone(location.getOfficePhone());
+        return dto;
+    }
+
+    @Override
+    public void add(AddLocationRequest addLocationRequest) {
+        Location location = new Location();
+        location.setCity(addLocationRequest.getCity());
+        location.setBranchOffice(addLocationRequest.getBranchOffice());
+        location.setOfficePhone(addLocationRequest.getOfficePhone());
+        locationRepository.save(location);
+    }
+
+    @Override
+    public void update(UpdateLocationRequest updateLocationRequest) {
+        Location locationToUpdate = locationRepository.findById(updateLocationRequest.getId()).orElseThrow();
+        locationToUpdate.setCity(updateLocationRequest.getCity());
+        locationToUpdate.setBranchOffice(updateLocationRequest.getBranchOffice());
+        locationToUpdate.setOfficePhone(updateLocationRequest.getOfficePhone());
+        locationRepository.save(locationToUpdate);
+    }
+
+    @Override
+    public void delete(int id) {
+        locationRepository.deleteById(id);
+    }
+
+}
