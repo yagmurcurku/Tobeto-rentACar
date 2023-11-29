cdaf6e4c89a74f630b7b20bad242c06095cf0c66
Yagmur Curku <yagmurcurku@gmail.com>
2023-11-29 21:47:04 +0300

    created ModelService

---
+++ src/main/java/com/tobeto/rentACar/services/abstracts/ModelService.java
@@ -0,0 +1,18 @@
+package com.tobeto.rentACar.services.abstracts;
+
+import com.tobeto.rentACar.services.dtos.requests.model.AddModelRequest;
+import com.tobeto.rentACar.services.dtos.requests.model.UpdateModelRequest;
+import com.tobeto.rentACar.services.dtos.responses.model.GetModelListResponse;
+import com.tobeto.rentACar.services.dtos.responses.model.GetModelResponse;
+
+import java.util.List;
+
+public interface ModelService {
+
+    List<GetModelListResponse> getAll();
+    GetModelResponse getById(int id);
+    void add(AddModelRequest addModelRequest);
+    void update(UpdateModelRequest updateModelRequest);
+    void delete(int id);
+
+}
