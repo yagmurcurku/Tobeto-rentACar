5b56e6bf8d664d3a354489ef1e88bb9662c717e2
Yagmur Curku <yagmurcurku@gmail.com>
2023-11-29 22:11:01 +0300

    created ModelManager

--- src/main/java/com/tobeto/rentACar/controllers/ModelsController.java
+++ src/main/java/com/tobeto/rentACar/controllers/ModelsController.java
@@ -1,11 +1,13 @@
 package com.tobeto.rentACar.controllers;
 
+import com.tobeto.rentACar.services.abstracts.ModelService;
 import com.tobeto.rentACar.services.dtos.requests.model.AddModelRequest;
 import com.tobeto.rentACar.services.dtos.requests.model.UpdateModelRequest;
 import com.tobeto.rentACar.services.dtos.responses.model.GetModelListResponse;
 import com.tobeto.rentACar.services.dtos.responses.model.GetModelResponse;
 import com.tobeto.rentACar.entities.Model;
 import com.tobeto.rentACar.repositories.ModelRepository;
+import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
@@ -13,63 +15,34 @@ import java.util.List;
 
 @RestController
 @RequestMapping("api/models")
+@AllArgsConstructor
 public class ModelsController {
 
-    private final ModelRepository modelRepository;
-
-    public ModelsController(ModelRepository modelRepository){
-        this.modelRepository=modelRepository;
-    }
+    private final ModelService modelService;
 
     @GetMapping
     public List<GetModelListResponse> getAll(){
-        List<Model> modelList = modelRepository.findAll();
-        List<GetModelListResponse> modelListResponses = new ArrayList<GetModelListResponse>();
-        for (Model model: modelList) {
-            GetModelListResponse modelResponse = new GetModelListResponse();
-            modelResponse.setId(model.getId());
-            modelResponse.setName(model.getName());
-            modelResponse.setYear(model.getYear());
-            modelResponse.setBrand(model.getBrand());
-            modelListResponses.add(modelResponse);
-        }
-        return modelListResponses;
+        return modelService.getAll();
     }
 
     @GetMapping("{id}")
     public GetModelResponse getById(@PathVariable int id){
-
-        Model model = modelRepository.findById(id).orElseThrow();
-        GetModelResponse dto = new GetModelResponse();
-        dto.setName(model.getName());
-        dto.setYear(model.getYear());
-        dto.setBrand(model.getBrand());
-        return dto;
-
+        return modelService.getById(id);
     }
 
     @PostMapping
     public void add(@RequestBody AddModelRequest addModelRequest){
-        Model model = new Model();
-        model.setName(addModelRequest.getName());
-        model.setYear(addModelRequest.getYear());
-        model.setBrand(addModelRequest.getBrand());
-        modelRepository.save(model);
+        modelService.add(addModelRequest);
     }
 
     @PutMapping
     public void update(@RequestBody UpdateModelRequest updateModelRequest){
-        Model modelToUpdate = modelRepository.findById(updateModelRequest.getId()).orElseThrow();
-        modelToUpdate.setName(updateModelRequest.getName());
-        modelToUpdate.setBrand(updateModelRequest.getBrand());
-        modelToUpdate.setYear(updateModelRequest.getYear());
-        //modelToUpdate.setCars(updateModelRequest.getCars());
-        modelRepository.save(modelToUpdate);
+        modelService.update(updateModelRequest);
     }
 
     @DeleteMapping("{id}")
     public void delete(@PathVariable int id){
-        modelRepository.deleteById(id);
+        modelService.delete(id);
     }
 
 
---
+++ src/main/java/com/tobeto/rentACar/services/concretes/ModelManager.java
@@ -0,0 +1,70 @@
+package com.tobeto.rentACar.services.concretes;
+
+import com.tobeto.rentACar.entities.Model;
+import com.tobeto.rentACar.repositories.ModelRepository;
+import com.tobeto.rentACar.services.abstracts.ModelService;
+import com.tobeto.rentACar.services.dtos.requests.model.AddModelRequest;
+import com.tobeto.rentACar.services.dtos.requests.model.UpdateModelRequest;
+import com.tobeto.rentACar.services.dtos.responses.model.GetModelListResponse;
+import com.tobeto.rentACar.services.dtos.responses.model.GetModelResponse;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+@AllArgsConstructor
+public class ModelManager implements ModelService {
+
+    private final ModelRepository modelRepository;
+
+    @Override
+    public List<GetModelListResponse> getAll() {
+        List<Model> modelList = modelRepository.findAll();
+        List<GetModelListResponse> modelListResponses = new ArrayList<GetModelListResponse>();
+        for (Model model: modelList) {
+            GetModelListResponse modelResponse = new GetModelListResponse();
+            modelResponse.setId(model.getId());
+            modelResponse.setName(model.getName());
+            modelResponse.setYear(model.getYear());
+            modelResponse.setBrand(model.getBrand());
+            modelListResponses.add(modelResponse);
+        }
+        return modelListResponses;
+    }
+
+    @Override
+    public GetModelResponse getById(int id) {
+        Model model = modelRepository.findById(id).orElseThrow();
+        GetModelResponse dto = new GetModelResponse();
+        dto.setName(model.getName());
+        dto.setYear(model.getYear());
+        dto.setBrand(model.getBrand());
+        return dto;
+    }
+
+    @Override
+    public void add(AddModelRequest addModelRequest) {
+        Model model = new Model();
+        model.setName(addModelRequest.getName());
+        model.setYear(addModelRequest.getYear());
+        model.setBrand(addModelRequest.getBrand());
+        modelRepository.save(model);
+    }
+
+    @Override
+    public void update(UpdateModelRequest updateModelRequest) {
+        Model modelToUpdate = modelRepository.findById(updateModelRequest.getId()).orElseThrow();
+        modelToUpdate.setName(updateModelRequest.getName());
+        modelToUpdate.setBrand(updateModelRequest.getBrand());
+        modelToUpdate.setYear(updateModelRequest.getYear());
+        modelRepository.save(modelToUpdate);
+    }
+
+    @Override
+    public void delete(int id) {
+        modelRepository.deleteById(id);
+    }
+
+}
