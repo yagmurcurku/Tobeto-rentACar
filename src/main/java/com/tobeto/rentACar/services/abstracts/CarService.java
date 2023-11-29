ae8b533d152dc12cbe149af7bc2af3d091d81c93
Yagmur Curku <yagmurcurku@gmail.com>
2023-11-29 20:53:04 +0300

    created CarService

---
+++ src/main/java/com/tobeto/rentACar/services/abstracts/CarService.java
@@ -0,0 +1,19 @@
+package com.tobeto.rentACar.services.abstracts;
+
+import com.tobeto.rentACar.entities.Car;
+import com.tobeto.rentACar.services.dtos.requests.car.AddCarRequest;
+import com.tobeto.rentACar.services.dtos.requests.car.UpdateCarRequest;
+import com.tobeto.rentACar.services.dtos.responses.car.GetCarListResponse;
+import com.tobeto.rentACar.services.dtos.responses.car.GetCarResponse;
+
+import java.util.List;
+
+public interface CarService {
+
+    List<GetCarListResponse> getAll();
+    GetCarResponse getById(int id);
+    void add(AddCarRequest addCarRequest);
+    void update(UpdateCarRequest updateCarRequest);
+    void delete(int id);
+
+}
